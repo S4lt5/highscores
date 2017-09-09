0ceab4510e0df83b567944b4a6faac08655f0593
MOTIV8N <matt@motiv8n.com>
2017-09-09 19:17:26 -0400

    Everything in there, needs a environment variable for the score db user and password but toherwise GTG

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var db = require('mongoose');
 
 
+
 var scoreSchema = db.Schema(
   {    
     rank: Number,
@@ -13,7 +14,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-db.connect("mongodb://highscores:highscores_pw@ds127994.mlab.com:27994/highscores");
+
+  //Set these environment variables to connect to DB
+  dbuser = process.env.DBUSER;
+  dbpass = process.env.DBPASS;
+db.connect("mongodb://" + dbuser + ":" + dbpass +  "@ds127994.mlab.com:27994/highscores");
   var Score = db.model("scores", scoreSchema);  
   var query = Score.find({}).sort("-score").limit(10);
   query.exec(function (err,scores)
@@ -28,6 +33,18 @@ db.connect("mongodb://highscores:highscores_pw@ds127994.mlab.com:27994/highscore
   });
   
   
+});
+
+router.post('/', function(request,res,next)
+{
+  console.log(request.body);
+    dbuser = process.env.DBUSER;
+  dbpass = process.env.DBPASS;
+  db.connect("mongodb://" + dbuser + ":" + dbpass +  "@ds127994.mlab.com:27994/highscores");
+  var Score = db.model("scores", scoreSchema);  
+  var score = new Score({name: request.body.name, score: request.body.score});
+  score.save();
+  res.send();
 });
 
 module.exports = router;
